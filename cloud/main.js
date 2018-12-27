2d5121262e48e5c02d4d57d92e57f0df7aeb31df
Roldão Junior <roldaogjr@gmail.com>
2018-12-26 21:19:26 -0300

    Ajustes nas funções Cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -2,48 +2,61 @@ const Contrato = Parse.Object.extend("Contrato")
 const Parcela = Parse.Object.extend("Parcela")
 
 async function contratosAtivosQuery() {
-	const query = new Parse.Query(Contrato)
-	query.equalTo("ativo", true)
-	return query.find()
+    const query = new Parse.Query(Contrato)
+    query.equalTo("ativo", true)
+    return query.find()
 }
 
 async function parcelasDoMesQuery(ids) {
-	var hoje = new Date(), mes = hoje.getMonth(), ano = hoje.getFullYear()
+    var hoje = new Date(), mes = hoje.getMonth(), ano = hoje.getFullYear()
 
-	const query = new Parse.Query(Parcela)
-	query.greaterThanOrEqualTo("vencimento", new Date(ano, mes, 1))
-	query.lessThan("vencimento", new Date(ano, mes+1, 1))
-	query.descending("vencimento")
-	query.containedIn("contrato", ids)
-	return query.find()
+    const query = new Parse.Query(Parcela)
+    query.greaterThanOrEqualTo("vencimento", new Date(ano, mes, 1))
+    query.lessThan("vencimento", new Date(ano, mes+1, 1))
+    query.containedIn("contrato", ids)
+    return query.find()
 }
 
 function criarParcela(contrato) {
-	let parcela = new Parcela()
-	var hoje = new Date(), mes = hoje.getMonth(), ano = hoje.getFullYear()
-	let ultimoDia = new Date(ano, mes+1, 0).getDate()
-	parcela.set("contrato", contrato.id)
-	parcela.set("vencimento", new Date(ano, mes, Math.min(ultimoDia, contrato.get("dia_vencimento"))))
-	parcela.set("valor", contrato.get("valor_mensal"))
-	return parcela.save()
+    var hoje = new Date(), mes = hoje.getMonth(), ano = hoje.getFullYear()
+    let ultimoDia = new Date(ano, mes+1, 0).getDate()
+    console.log("Criando parcela para o contrato",
+                contrato.id, "mês", mes+"/"+ano)
+
+    let vencimento = new Date(ano, mes, Math.min(ultimoDia, contrato.get("dia_vencimento")))
+    if (vencimento < hoje) vencimento = hoje
+    let parcela = new Parcela()
+    parcela.set("contrato", contrato.id)
+    parcela.set("vencimento", vencimento)
+    parcela.set("valor", contrato.get("valor_mensal"))
+    parcela.set("pago", false)
+    return parcela.save()
+}
+
+async function contratosComParcelaQuery(ids) {
+    return parcelasDoMesQuery(ids).then(parcelas => (
+        parcelas.map(it => it.get('contrato'))
+    ))
 }
 
 Parse.Cloud.job("atualizarParcelas", async (request) => {
-	let contratos = await contratosAtivosQuery()
-	let paercelas = await parcelasDoMesQuery(contratos.map(it => it.id))
-	let contratos_com_parcela = paercelas.map(it => it.get('contrato'))
-	let novas_parcelas = []
-	let parcelas = []
+    let contratos = await contratosAtivosQuery()
+    let contratos_com_parcela = await contratosComParcelaQuery(
+        contratos.map(it => it.id))
 
-	contratos.forEach(contrato => {
-		if(!contratos_com_parcela.includes(contrato.id)) {
-			parcelas.push(criarParcela(contrato).then(p => novas_parcelas.push(p)))
-		}
-	})
-	await Promise.all(parcelas)
-	return {novas_parcelas: novas_parcelas}
+    contratos.forEach(contrato => {
+        if(!contratos_com_parcela.includes(contrato.id)) {
+            criarParcela(contrato)
+        }
+    })
+    return {}
 })
 
-Parse.Cloud.afterSave("Contrato", (request) => {
-    criarParcela(request.object)
+Parse.Cloud.afterSave("Contrato", async (request) => {
+    if(request.object.get("ativo")) {
+        let contratos = await contratosComParcelaQuery([request.object.id])
+        if(!contratos.includes(request.object.id)) {
+            criarParcela(request.object)
+        }
+    }
 })
