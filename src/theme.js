0089adb83cd8196ada69fe97343fa5f9ac467830
Roldão Rego Jr <roldaogjr@gmail.com>
2019-03-02 11:03:59 -0300

    Atualizado tema material

--- src/theme.js
+++ src/theme.js
@@ -1,14 +1,9 @@
-import { createMuiTheme } from '@material-ui/core/styles'
-import orange from '@material-ui/core/colors/orange'
+import { createMuiTheme } from '@material-ui/core/styles';
 
-export default createMuiTheme({
-    palette: {
-        primary: {
-            main: orange[800],
-        },
-        secondary: {
-            main: orange[700],
-        },
-        contrastThreshold: 2
-    }
-})
+const palette = {
+  primary: { main: '#F57C00', contrastText: '#FAFAFA' },
+  secondary: { main: '#FF9800', contrastText: '#FAFAFA' }
+};
+const themeName = 'Gold Drop Pizazz Cow';
+
+export default createMuiTheme({ palette, themeName });
